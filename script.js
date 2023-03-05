3d77ed20e920e38e06c8d8188ecc747b875514cf
Erema-sib <eremasib@mail.ru>
2023-03-06 01:52:01 +0700

    Добавлен блок с фото и текстом, доработана дата и время

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+const days = [
+   "Вс",
+   "Пн",
+   "Вт",
+   "Ср",
+   "Чт",
+   "Пт",
+   "Сб"
+];
+
+
+
 window.onload = function() {
     window.setInterval(function() {
        let date = new Date();
        
+       let num = date.getDate();
+       let day_week = date.getDay();
        let hours = date.getHours();
        let minutes = date.getMinutes();
-       let seconds = date.getSeconds();
-
+       let sec = date.getSeconds();
+       
+       if (num < 10) num = "0" + num;
        if(hours < 10) hours = "0" + hours;
        if(minutes < 10) minutes = "0" + minutes;
-       if(seconds < 10) seconds = "0" + seconds;
-
-       const clock = hours + ":" + minutes + ":" + seconds;
+       
+       const clock = num + " " + days[day_week] + ", " + (sec%2 ? `${hours}:${minutes}` : `${hours} ${minutes}`);
        document.getElementById("clock").innerHTML = clock;
+
     }, 1000);
  };
